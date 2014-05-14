0294a7a6de85d4ed12eb57be807bf047f17551ed
bope6555 <bogdan.petkov@colorado.edu>
2014-05-14 14:10:52 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+['chrome', 'firefox'].forEach(detectBrowser);
+
+function detectBrowser(name) {
+  require('webdriverjs')
+    .remote({
+      desiredCapabilities: {
+        browserName: name
+      }
+    })
+    .init()
+    .url('http://www.alpine.gridcraft.com/')
+    .getText('.detected.simple-browser-string', function(err, text) {
+      console.log(name + 'browser was detected as ' + text);
+    })
+    .end()
+}
