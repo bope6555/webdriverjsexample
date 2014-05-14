98ad848845324aa615fc15cc3e1b131c1346d25c
bope6555 <bogdan.petkov@colorado.edu>
2014-05-14 14:41:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function detectBrowser(name) {
     .init()
     .url('http://www.alpine.gridcraft.com/')
     .getText('.detected.simple-browser-string', function(err, text) {
-      setTimeout(function(){alert("hi")}, 5000);
+      sleep(5000);
       console.log(name + 'browser was detected as ' + text);
     })
     .end()
